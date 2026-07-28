2e2cfe7fe7aa56c0768a9d4c866b7a37b081b435
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-07-28 15:24:35 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+set -e
+
+OUTPUT="dawn"
+
+SOURCES=(
+    "src/main.cpp"
+    "src/core/engine.cpp"
+    "src/utils/network.cpp"
+)
+
+CXXFLAGS="-std=c++20 -Isrc -Wall -Wextra"
+LDFLAGS="-lcurl"
+
+if g++ $CXXFLAGS "${SOURCES[@]}" -o "$OUTPUT" $LDFLAGS;
+{
+    echo "Compilation successful: ./$OUTPUT"
+}
+else
+{
+    echo "Compilation failed."
+    exit 1
+}
